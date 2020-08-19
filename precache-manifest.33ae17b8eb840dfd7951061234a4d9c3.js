afdbf0cba64aa6daf4af8dfdcfbe52118c074f02
Kathiresan, Pradeepa <pradeepa.kathiresan@accenture.com>
2020-08-20 02:13:32 +0530

    Updates

---
+++ precache-manifest.33ae17b8eb840dfd7951061234a4d9c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798dd5848f84b23f9f2d3cd63e84a9cb",
+    "url": "/reactCV/index.html"
+  },
+  {
+    "revision": "0754ef0158950152b9b8",
+    "url": "/reactCV/static/css/main.51544323.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/reactCV/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/reactCV/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0754ef0158950152b9b8",
+    "url": "/reactCV/static/js/main.0670b155.chunk.js"
+  },
+  {
+    "revision": "5f9b8403f63bbabccf6d",
+    "url": "/reactCV/static/js/runtime-main.8d6539c9.js"
+  },
+  {
+    "revision": "9e509640e3308c20153f37aaea52db4a",
+    "url": "/reactCV/static/media/deepa_photo.9e509640.jpg"
+  }
+]);
